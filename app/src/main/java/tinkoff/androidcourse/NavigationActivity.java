54ded00a5d6dd5f05fd1d3bef29fbaac5066429d
Andrew Yefanov <1134togo@gmail.com>
2017-03-30 15:50:59 +0300

    Очистка бекстека, когда фрагмент добавляется из NavigationBar

--- app/src/main/java/tinkoff/androidcourse/NavigationActivity.java
+++ app/src/main/java/tinkoff/androidcourse/NavigationActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -94,10 +95,13 @@ public class NavigationActivity extends AppCompatActivity
     }
 
     private void setFragment(Fragment fragment) {
-        getSupportFragmentManager().beginTransaction()
-                                   .setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
-                                   .replace(R.id.content_navigation, fragment)
-                                   .commit();
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        if (supportFragmentManager.getBackStackEntryCount() > 0)
+            supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        supportFragmentManager.beginTransaction()
+                              .setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+                              .replace(R.id.content_navigation, fragment)
+                              .commit();
     }
 
     private void addFragmentOnBackStack(Fragment fragment) {
